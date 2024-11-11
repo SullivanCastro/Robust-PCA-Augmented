cc4667896f17e4f5c597729894316239a3099cd7
SullivanCastro <sullivan.castro@eleves.enpc.fr>
2024-11-11 15:41:27 +0100

    improve video result display

--- Datasets/remove_background.py
+++ Datasets/remove_background.py
@@ -30,17 +30,41 @@ def load_dataset(path:str) -> np.array:
                 datasets[idx] = image.flatten()
     return datasets
 
+
+def normalize_image(image: np.array) -> np.array:
+    """
+    Normalize the image
+
+    Parameters
+    ----------
+    image : np.array
+        Image to normalize
+
+    Returns
+    -------
+    np.array
+        Normalized image
+    """
+    return (image - np.min(image)) / (np.max(image) - np.min(image))
+
 dataset_cyprien = load_dataset("/Users/sullivancastro/Desktop/MVA/Geometric Data Analysis/Robust-PCA-Augmented/Datasets/City")
 print(dataset_cyprien.shape)
 
 
 rpca = Robust_PCA(dataset_cyprien)
 L, S = rpca.fit()
-image = L.mean(axis=0).astype(np.uint8).reshape((180, 320)).astype(np.uint8)
-image = Image.fromarray(image, mode="L")
+lr_image = normalize_image(L.mean(axis=0)).astype(np.uint8).reshape((180, 320)).astype(np.uint8)
+lr_image = Image.fromarray(lr_image, mode="L")
 
-image.save("/Users/sullivancastro/Desktop/MVA/Geometric Data Analysis/Robust-PCA-Augmented/results.png")
-plt.figure(figsize=(10, 5))
-plt.imshow(image, cmap="gray")
-plt.title("Low-rank matrix")
+s_image = normalize_image(S.mean(axis=0)).astype(np.uint8).reshape((180, 320)).astype(np.uint8)
+s_image = Image.fromarray(s_image, mode="L")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+
+ax[0, 0].imshow(lr_image, cmap="gray")
+ax[0, 0].set_title("Low-rank matrix")
+
+ax[0, 1].imshow(S.mean(axis=0).reshape((180, 320)), cmap="gray")
+ax[0, 1].set_title("Sparse matrix")
+
 plt.show()
