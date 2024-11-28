97772053218ea79a47a494ac13315f0ab476d1c5
LiBekka <lina.bekkali@eleves.enpc.fr>
2024-11-28 21:21:26 +0100

    clustering_tests

--- model_custom_pca/rpca_augmented.py
+++ model_custom_pca/rpca_augmented.py
@@ -1,31 +1,33 @@
 import numpy as np
 from sklearn.cluster import KMeans
 from scipy.linalg import svd
-from scipy.sparse.csgraph import laplacian
 from sklearn.neighbors import kneighbors_graph
+from sklearn.decomposition import PCA
 from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import pairwise_distances
+from sklearn.metrics import accuracy_score
+from scipy.optimize import linear_sum_assignment as hungarian
 import json
 from sklearn.preprocessing import StandardScaler
 
 
 class Robust_PCA_augmented():
 
-    def __init__(self, M, gamma=0) -> None:
-        scaler = StandardScaler()
-        M = scaler.fit_transform(M)
+    def __init__(self, M, corruption=False, annotation_path = None, gamma=10, nb_iter=100, knn=0) -> None:
         self.M           = M
         self.k           = 0
         self.gamma       = gamma
         self.r1          = 1
         self.r2          = 1
+        self.annotation_path = annotation_path
         self.epsilon     = 1e-7
         self._lambda     = 1/np.sqrt(np.max(M.shape))
-        self.n_neighbors = 10
-        self.corruption  = False
+        self.corruption  = corruption
+        self.nb_iter     = nb_iter
         self.corruption_type = 'occlusion'
+        self.knn        = knn
 
-        self.laplacien = self.laplacian_computation(self.M)
+        self.laplacien = self.laplacian_computation()
 
         self.L           = np.zeros(M.shape)
         self.W           = np.zeros(M.shape)
@@ -36,7 +38,7 @@ class Robust_PCA_augmented():
         self.P2          = self._lambda * np.sum(np.abs(self.S))
         self.P3          = self.gamma * np.trace(self.L.T @ self.laplacien @ self.L)
 
-    def pairwaise_distance(self, X):
+    def custom_pairwise_distance(self):
         """
         Compute the pairwise distance between the data points
         When the corruption is set to occlusion
@@ -52,23 +54,38 @@ class Robust_PCA_augmented():
         np.array
             Pairwise distance matrix
         """
-        O = np.zeros((X.shape[0], X.shape[0]))
-        annotation_dataset = json.load(open("C:\\MVA\\1er Semestre\\G Data Analysis\\RPCA\\Robust-PCA-Augmented\\Corrupted_Datasets\\occlusion\\Cyprien\\annotation.json"), "r")
-        if self.corruption_type == 'occlusion':
-            for i in range(X.shape[0]):
-                for j in range(X.shape[0]):
-                    image_first_pixel, image_patch_size = annotation_dataset[str(i)]
-                    image_first_pixel2, image_patch_size2 = annotation_dataset[str(j)]
-                    mask = np.ones((X.shape[1], X.shape[1]))
-                    mask[image_first_pixel[0]:image_first_pixel[0] + image_patch_size, image_first_pixel[1]:image_first_pixel[1] + image_patch_size] = 0
-                    mask[image_first_pixel2[0]:image_first_pixel2[0] + image_patch_size2, image_first_pixel2[1]:image_first_pixel2[1] + image_patch_size2] = 0
+        num_samples = self.M.shape[0]
+        O = np.zeros((num_samples, num_samples))
+        with open(self.annotation_path, "r") as file:
+            annotation_dataset = json.load(file) 
 
-                    O[i, j] = np.linalg.norm(mask * (X[i] - X[j]), 2) / np.sum(mask)
-        
+        image_shape = (92, 112) 
+
+        if self.corruption_type == 'occlusion':
+            for i in range(self.M.shape[0]):
+                for j in range(self.M.shape[0]):
+                    if i == j:
+                        O[i, j] = 0
+                    else:
+                        (x1, y1), patch_size1 = annotation_dataset[str(i)]
+                        (x2, y2), patch_size2 = annotation_dataset[str(j)]
+
+                        image_i = self.M[i].reshape(image_shape)
+                        image_j = self.M[j].reshape(image_shape)
+
+                        mask = np.ones(image_shape)
+
+                        mask[x1:x1+patch_size1, y1:y1+patch_size1] = 0
+                        mask[x2:x2+patch_size2, y2:y2+patch_size2] = 0
+
+                        masked_image_i = (mask * image_i).flatten()
+                        masked_image_j = (mask * image_j).flatten()
+
+                        O[i, j] = np.linalg.norm(masked_image_i - masked_image_j, 2) / np.sum(mask)
+
         return O
 
-
-    def laplacian_computation(self, X):
+    def laplacian_computation(self):
         """
         Compute the laplacian matrix
 
@@ -82,27 +99,29 @@ class Robust_PCA_augmented():
         np.array
             Laplacian matrix
         """
-        # Library based technique to compute the laplacian matrix, working a priori
-        # knn_weights = kneighbors_graph(X, self.n_neighbors, mode='distance', include_self=True)
-        # knn_weights_square = np.square(knn_weights.toarray())        
-        # self.laplacien = laplacian(knn_weights_square, normed=False)
 
-        # Manual technique to compute the laplacian matrix
         if not self.corruption:
-            distances = pairwise_distances(X, metric='euclidean')
+            distances = pairwise_distances(self.M, metric='euclidean')
         else:
-            distances = self.pairwaise_distance(X)
+            distances = self.custom_pairwise_distance()
 
-        nbrs = NearestNeighbors(n_neighbors=self.n_neighbors, algorithm='ball_tree').fit(X)
-        _, indices = nbrs.kneighbors(X)
-        A = np.zeros((X.shape[0], X.shape[0]))
-        omega = np.min(distances[distances != 0])
-        for i in range(X.shape[0]):
-            for j in indices[i]:
-                A[i, j] = np.exp(-(distances[i, j] - omega) ** 2 / 0.05)
-            A = (A + A.T) / 2
-        D_inv_sqrt = np.diag(1.0 / np.sqrt(A.sum(axis=1) + 1e-10))
-        self.laplacien = np.eye(X.shape[0]) - D_inv_sqrt @ A @ D_inv_sqrt
+        A = np.zeros((self.M.shape[0], self.M.shape[0]))
+        omega = np.min(distances[distances > 0])
+        if self.knn == 0:
+            for i in range(self.M.shape[0]):
+                for j in range(self.M.shape[0]):
+                    A[i, j] = np.exp(-(distances[i, j] - omega) ** 2 / 0.05)
+        else:
+            for i in range(self.M.shape[0]):
+                idx = np.argsort(distances[i])[:self.knn]
+                for j in idx:
+                    A[i, j] = np.exp(-(distances[i, j] - omega) ** 2 / 0.05)
+
+        A = (A + A.T) / 2
+        
+        D = np.diag(np.sum(A, axis=1))
+        D_inv_sqrt = np.diag(1.0 / np.sqrt(np.diag(D) + 1e-10))
+        self.laplacien = np.eye(self.M.shape[0]) - D_inv_sqrt @ A @ D_inv_sqrt
 
         return self.laplacien
 
@@ -208,12 +227,12 @@ class Robust_PCA_augmented():
             S matrix
         """
 
-        P1_prev, P2_prev, P3_prev = self.P1, self.P2, self.P3
-        Z1_prev, Z2_prev = self.Z1, self.Z2
+        # P1_prev, P2_prev, P3_prev = self.P1, self.P2, self.P3
+        # Z1_prev, Z2_prev = self.Z1, self.Z2
         
-        while self.k < 100 or (np.square(self.P1 - P1_prev) / np.square(P1_prev) > self.epsilon and np.square(self.P2- P2_prev) / np.square(P2_prev) > self.epsilon and np.square(self.P3 - P3_prev) / np.square(P3_prev) > self.epsilon and self.stopping_criterion(self.Z1, Z1_prev) and self.stopping_criterion(self.Z2, Z2_prev)):
-            P1_prev, P2_prev, P3_prev = self.P1, self.P2, self.P3
-            Z1_prev, Z2_prev = self.Z1, self.Z2
+        while self.k < self.nb_iter :
+            # P1_prev, P2_prev, P3_prev = self.P1, self.P2, self.P3
+            # Z1_prev, Z2_prev = self.Z1, self.Z2
             self.L = self._compute_L_next()
             self.S = self._compute_S_next()
             self.W = self._compute_W_next()
@@ -224,9 +243,22 @@ class Robust_PCA_augmented():
             self.P3 = self.gamma * np.trace(self.L.T @ self.laplacien @ self.L)
             self.k += 1
             print(f"Iteration {self.k} - P1: {self.P1} - P2: {self.P2} - P3: {self.P3}, Difference between S and L: {np.linalg.norm(self.S - self.L)}")
+        
         return self.L, self.S
+    
+    def compute_clustering_error(self, labels, y_true):
+        cost_matrix = np.zeros((np.max(labels) + 1, np.max(y_true) + 1))
+        for i in range(len(labels)):
+            cost_matrix[labels[i], y_true[i]] += 1
 
-    def pca_predict(self, k, true_labels):
+        row_ind, col_ind = hungarian(-cost_matrix)
+        mapping = dict(zip(row_ind, col_ind))
+
+        aligned_labels = np.array([mapping[label] for label in labels])
+        return 100 * (1 - accuracy_score(y_true, aligned_labels))
+
+
+    def pca_predict(self, true_labels):
         """
         Test the pca model for clustering data
 
@@ -236,14 +268,12 @@ class Robust_PCA_augmented():
             Clustering error
         """
         U, S, V = np.linalg.svd(self.L, full_matrices=False)
-        for i in range(10):
-            kmeans = KMeans(n_clusters=k, n_init=10).fit(U[:, :k])
-            if i == 0:
-                min_error = np.sum(kmeans.labels_ != true_labels) / len(true_labels) * 100
-            else:
-                error = np.sum(kmeans.labels_ != true_labels) / len(true_labels) * 100
-                if error < min_error:
-                    min_error = error
-        return min_error
+        cumulated_variance = np.cumsum(S**2) / np.sum(S**2)
+        n_components = np.argmax(cumulated_variance > 0.95) + 1
 
-
+        errors = []
+        for _ in range(10):
+            kmeans = KMeans(n_clusters=len(np.unique(true_labels)), n_init=10, random_state=None)
+            kmeans.fit(U[:, :n_components])
+            errors.append(self.compute_clustering_error(kmeans.labels_, true_labels))
+        return np.min(errors)
